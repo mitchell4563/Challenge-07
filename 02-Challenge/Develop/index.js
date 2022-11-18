c7f451be7d1393c15d6a870fa673b06faf290b94
Mitchell Wood <mitchellwood11@gmail.com>
2022-11-18 13:35:15 -0600

    questions and init function integrated

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -1,14 +1,65 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-inquirer
+const fs = require('fs')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Project Title: ',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter installation instructions: '
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Project Description: '
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Describe the usage of your project: '
+    },
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'Enter any contributions to your project: '
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email: '
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your Github username: '
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select your license: ',
+        choices: ['MIT', 'ISC', 'GNUPLv3'],
+        filter(val) {
+            return val.toLowerCase();
+        }
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions)
+        .then((data) => {
+            console.log(data)
+            return data
+        })
+}
 
 // Function call to initialize app
 init();
