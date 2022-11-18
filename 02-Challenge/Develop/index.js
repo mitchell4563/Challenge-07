2de75d86df1d123c66f89cc47810d5c8c7af4b3e
Mitchell Wood <mitchellwood11@gmail.com>
2022-11-18 14:59:25 -0600

    markdown/file writing created

--- 02-Challenge/Develop/index.js
+++ 02-Challenge/Develop/index.js
@@ -1,64 +1,67 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs')
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'Project Title: ',
+  {
+    type: "input",
+    name: "title",
+    message: "Project Title: ",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please enter installation instructions: ",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Project Description: ",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Describe the usage of your project: ",
+  },
+  {
+    type: "input",
+    name: "contributions",
+    message: "Enter any contributions to your project: ",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please enter your email: ",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Please enter your Github username: ",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Please select your license: ",
+    choices: ["MIT", "ISC", "GNUPLv3"],
+    filter(val) {
+      return val.toLowerCase();
     },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Please enter installation instructions: '
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Project Description: '
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Describe the usage of your project: '
-    },
-    {
-        type: 'input',
-        name: 'contributions',
-        message: 'Enter any contributions to your project: '
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Please enter your email: '
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'Please enter your Github username: '
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'Please select your license: ',
-        choices: ['MIT', 'ISC', 'GNUPLv3'],
-        filter(val) {
-            return val.toLowerCase();
-        }
-    },
+  },
 ];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-    return inquirer.prompt(questions)
-        .then((data) => {
-            console.log(data)
-            return data
-        })
+  return inquirer.prompt(questions).then((data) => {
+    const readME = generateMarkdown(data);
+    fs.writeFile("README.md", readME, function (err) {
+      if (err) {
+        console.log("Could not save file");
+      } else {
+        console.log("Success!");
+      }
+    });
+  });
 }
 
 // Function call to initialize app
--- 02-Challenge/Develop/utils/generateMarkdown.js
+++ 02-Challenge/Develop/utils/generateMarkdown.js
@@ -1,18 +1,68 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  const badges = {
+    mit: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+    isc: '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)',
+    gnugplv3: '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)'
+  }
+  return badges[license]
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  const licenseLinks = {
+    mit: '[MIT](https://choosealicense.com/licenses/mit/)',
+    isc: '[ISC](https://choosealicense.com/licenses/isc/)',
+    gnugplv3: '[GNUGPLv3](https://choosealicense.com/licenses/gpl-3.0/)'
+  }
+  return licenseLinks[license]
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license) {
+    return `Licensed under the ${renderLicenseLink(license)} license`
+  } else {
+    return ''
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
+
+${renderLicenseBadge(data.license)}
+
+## Table of Contents
+- [Project Description](#Description)
+- [Usage](#Usage)
+- [Contributions](#Contributions)
+- [Installation](#Installation)
+- [Questions](#Questions)
+- [License](#License)
+
+## Description
+${data.description}
+
+## Usage
+${data.usage}
+
+## Installation
+${data.installation}
+
+## Contributions
+${data.contributions}
+
+## Questions
+Email - ${data.email}<br>
+Github - ${data.github}
+
+## License
+${renderLicenseSection(data.license)}
 
 `;
 }
